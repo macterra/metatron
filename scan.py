524c2651ca2127510381c1871d1a1b36107103ca
David McFadzean <davidmc@gmail.com>
2021-03-14 11:20:49 -0400

    Retrieve last 10 blocks

--- scan.py
+++ scan.py
@@ -1,11 +1,21 @@
 
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from datetime import datetime
+from dateutil import tz
 
-# credentials should export a connect string like "http://user:password@server:port"
+# credentials should export a connect string like "http://rpc_user:rpc_password@server:port"
+# rpc_user and rpc_password are set in the bitcoin.conf file
 from credentials import connect
     
-# rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy(connect, timeout=120)
-best_block_hash = rpc_connection.getbestblockhash()
-tip = rpc_connection.getblock(best_block_hash)
-print(tip)
+block_count = rpc_connection.getblockcount()
+
+for height in range(block_count, block_count-10, -1):
+    block_hash = rpc_connection.getblockhash(height)
+    block = rpc_connection.getblock(block_hash)
+    
+    block_time = block['time']
+    utc = datetime.utcfromtimestamp(block_time).replace(tzinfo=tz.tzutc())
+    timestamp = utc.astimezone(tz.tzlocal()).strftime('%Y-%m-%d %H:%M:%S')
+
+    print(height, block_hash, block_time, utc, timestamp)
