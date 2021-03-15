bdc664b9dbb3e6e2fc0518a2f4f720cf165afd1b
David McFadzean <davidmc@gmail.com>
2021-03-14 20:36:18 -0400

    Recover CID from txn data

--- scan.py
+++ scan.py
@@ -1,7 +1,11 @@
 
+# pip install bitcoinrpc
+# pip install py-cid
+
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from datetime import datetime
 from dateutil import tz
+import cid
 
 # credentials should export a connect string like "http://rpc_user:rpc_password@server:port"
 # rpc_user and rpc_password are set in the bitcoin.conf file
@@ -10,7 +14,7 @@ from credentials import connect
 rpc_connection = AuthServiceProxy(connect, timeout=120)
 block_count = rpc_connection.getblockcount()
 
-for height in range(block_count, block_count-10, -1):
+def scanBlock(height):
     block_hash = rpc_connection.getblockhash(height)
     block = rpc_connection.getblock(block_hash)
     
@@ -18,12 +22,12 @@ for height in range(block_count, block_count-10, -1):
     utc = datetime.utcfromtimestamp(block_time).replace(tzinfo=tz.tzutc())
     timestamp = utc.astimezone(tz.tzlocal()).strftime('%Y-%m-%d %H:%M:%S')
 
+    #print(block)
     print(height, block_hash, block_time, utc, timestamp)
-    print(block)
 
     txns = block['tx']
     for txid in txns:
-        print(txid)
+        #print(txid)
         tx = rpc_connection.getrawtransaction(txid, 1)
         #print(tx)
         for vout in tx['vout']:
@@ -31,5 +35,13 @@ for height in range(block_count, block_count-10, -1):
             scriptPubKey = vout['scriptPubKey']
             script_type = scriptPubKey['type']
             if script_type == 'nulldata':
-                data = scriptPubKey['hex']
-                print("data>>", data)
+                hexdata = scriptPubKey['hex']
+                print("hex>>", hexdata)
+                data = bytes.fromhex(hexdata)
+                if data[0] == 0x6a and data[1] == 40:
+                    if data[2:6] == b'CID1':
+                        x = cid.make_cid(data[6:])
+                        print("CID1>>", x.encode())
+                        print("CID0>>", x.to_v0().encode())
+
+scanBlock(1940280)                    
