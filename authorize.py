490e9bca06f5b33c61956e23784fdefba6467de2
David McFadzean <davidmc@gmail.com>
2021-04-02 15:04:01 -0400

    txid a9154b8f7047e95e98e74c5007f3a7ad1d310d00f7d1339868d41fc2e588c100

--- authorize.py
+++ authorize.py
@@ -1,40 +1,69 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from cid import make_cid
 import binascii
+import json
+import ipfshttpclient
 
 # credentials should export a connect string like "http://rpc_user:rpc_password@server:port"
 # rpc_user and rpc_password are set in the bitcoin.conf file
 from credentials import connect
     
 rpc_connection = AuthServiceProxy(connect, timeout=120)
-block_count = rpc_connection.getblockcount()
+client = ipfshttpclient.connect()
 
-print(block_count)
+print(client.id())
 
-info = rpc_connection.getwalletinfo()
-print(info)
+with open("db.json", "r") as read_file:
+    db = json.load(read_file)
 
-newaddr = rpc_connection.getnewaddress('bech32')
-print(newaddr)
+print(db)
 
-prev = { "txid": "78e189eff6ce7b6437d207ff5c5727126a61b3d7d573ef61c450c54302d90bb0", "vout": 0 } 
+def authorize(filename):
+    res = client.add(filename)
+    print('res', res)
+    hashcid = res['Hash']
+    meta = json.loads(client.cat(hashcid))
+    print(meta)
 
-cid = make_cid("QmUZc1UF25Q5R6nu2W83Yi6RNREXjxD43T1xzSw9rxLrEp")
-scheme = binascii.hexlify(str.encode("CID1")).decode()
-print('cid', cid, scheme, cid.multihash.hex())
-output = { "data": scheme + cid.multihash.hex() }
+    xid = meta['id']
+    print('xid', xid)
 
-rawtxn = rpc_connection.createrawtransaction([prev], [output])
-print('raw', rawtxn)
+    if xid in db:
+        print('found xid', xid)
 
-funtxn = rpc_connection.fundrawtransaction(rawtxn)
-print('fun', funtxn)
+        certcid = db[xid]
+        print('certcid', certcid)
 
-sigtxn = rpc_connection.signrawtransactionwithwallet(funtxn['hex'])
-print('sig', sigtxn)
+        cert = json.loads(client.cat(certcid))
+        print('cert', cert)
+        print('verify xid', xid == cert['id'])
 
-dectxn = rpc_connection.decoderawtransaction(sigtxn['hex'])
-print('dec', dectxn)
+        txid = cert['tx']['txid']
+        print('txid', txid)
 
-txid = rpc_connection.sendrawtransaction(sigtxn['hex'])
-print('txid', txid)
+        prev = [{ "txid": txid, "vout": 0 }]
+    else:
+        print('first version of', xid)
+        prev = []
+
+    cid = make_cid(hashcid)
+    scheme = binascii.hexlify(str.encode("CID1")).decode()
+    print('cid', cid, scheme, cid.multihash.hex())
+    output = { "data": scheme + cid.multihash.hex() }
+
+    rawtxn = rpc_connection.createrawtransaction(prev, [output])
+    print('raw', rawtxn)
+
+    funtxn = rpc_connection.fundrawtransaction(rawtxn)
+    print('fun', funtxn)
+
+    sigtxn = rpc_connection.signrawtransactionwithwallet(funtxn['hex'])
+    print('sig', sigtxn)
+
+    dectxn = rpc_connection.decoderawtransaction(sigtxn['hex'])
+    print('dec', dectxn)
+
+    txid = rpc_connection.sendrawtransaction(sigtxn['hex'])
+    print('txid', txid)
+
+authorize('meta-v2.json')
