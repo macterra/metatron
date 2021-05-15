1422c8a1b0e88c4cc4698d16098af09a5d308cb1
David McFadzean <davidmc@gmail.com>
2021-05-15 19:54:53 -0400

    Added /cid route

--- webui/index.js
+++ webui/index.js
@@ -28,22 +28,33 @@ app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(fileUpload())
 
-app.get('/', (req,res) => {
-    db = "TBD"
-    res.render('home', {db})
-
-    // console.log('home hit')
-    // console.log(uuidv4())
-    
-    // cid = 'QmTpkUxYfEXg1ZzFVYGr62P78HAGN9TSV1v6vMGp333vcv'
-    // resolveCid(cid)
+app.get('/', (req, res) => {
+    getCerts().then(certs => {
+        status = JSON.stringify(certs, null, 2)
+        res.render('home', {certs})
+    })
 })
 
-app.get('/status', (req,res) => {
+app.get('/status', (req, res) => {
     getStatus().then(status => {
         status = JSON.stringify(status, null, 2)
         res.render('status', {status})
     })
+})
+
+app.get('/cid/:cid', (req, res) => {
+    console.log(req.params.cid)
+    const cid = req.params.cid
+
+    console.log('meta', cid)
+    const resolve = resolveCid(cid)
+    
+    resolve.then(data => {
+        console.log('certs', data.certs)
+        console.log('orig', data.orig)
+        rawcerts = JSON.stringify(data.certs, null, 2)
+        res.render('meta', {cid, data, rawcerts})     
+    })    
 })
 
 const getStatus = async () => {
@@ -57,17 +68,22 @@ const getStatus = async () => {
         status[key] = val
         console.log(key, val)
     }
-    
+
+    return status
+}
+
+const getCerts = async () => {
+    certs = []
+
     keys = await client.keysAsync("xid/*")
     console.log(keys)
 
     for (const key of keys.sort()) {
         val = await client.getAsync(key)
-        status[key] = val
-        console.log(key, val)
+        certs.push(val)
     }
 
-    return status
+    return certs
 }
 
 app.post('/meta', (req, res) => {
